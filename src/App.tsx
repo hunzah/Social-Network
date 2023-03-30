bfa3ebd9de3efa3cfe1ca5032e56d322407a5409
Yusup <hunzah16@gmail.com>
2023-03-30 08:05:38 +0300

    first steps

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
+import Technologies from './Technologies';
+import {Footer} from './Footer';
 
 function App() {
   return (
-    <div className="App">
-     Hello, samurai! Let's go!
+    <div>
+      <Header />
+      <Technologies />
+        <Footer/>
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/Technologies.tsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+const Technologies = () => {
+  return (
+    <div className="App">
+      Hello, samurai! Let's go!
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+      </ul>
+    </div>)
+}
+export default Technologies;
+
+
