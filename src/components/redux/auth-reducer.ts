13fc0e701e5d7d34e15e6312af3430afcf44dd4b
Yusup <hunzah9@mail.ru>
2023-06-21 20:38:49 +0300

    code refactoring

--- src/components/redux/auth-reducer.ts
+++ src/components/redux/auth-reducer.ts
@@ -2,6 +2,7 @@ import {ActionTypes, DispatchType} from './redux-store';
 import {authApi} from '../../api/api';
 import {stopSubmit} from 'redux-form';
 
+
 export type ResponseDataType<T> = {
     resultCode: number,
     messages: string[],
@@ -26,12 +27,12 @@ const initialState = {
 
 export const authReducer = (state: DataType = initialState, action: ActionTypes): DataType => {
     switch (action.type) {
-        case 'SET-USERS-DATA':
+        case 'auth/SET-USERS-DATA':
             return {
                 ...state,
                 ...action.payload, isAuth: action.payload.isAuth
             };
-        case 'SET-LOADING':
+        case 'auth/SET-LOADING':
             return {
                 ...state,
                 isLoading: action.isLoading
@@ -43,47 +44,43 @@ export const authReducer = (state: DataType = initialState, action: ActionTypes)
 
 
 export const SetUserDataAC = (id: string | null, email: string | null, login: string | null, isAuth: boolean) => ({
-    type: 'SET-USERS-DATA',
+    type: 'auth/SET-USERS-DATA',
     payload: {id, email, login, isAuth}
 } as const);
 
 export const SetLoadingAC = (isLoading: boolean) => ({
-    type: 'SET-LOADING',
+    type: 'auth/SET-LOADING',
     isLoading
 } as const);
 
 
-export const authUserThunk = () => {
-    return (dispatch: DispatchType) => {
-        authApi.me().then((data) => {
-            if (data.resultCode === 0) {
-                let {id, email, login} = data.data;
-                dispatch(SetUserDataAC(id, email, login, true));
-            }
-            dispatch(SetLoadingAC(false))
-        });
-    };
+export const authUserThunk = () => async (dispatch: DispatchType) => {
+    let response = await authApi.me()
+    if (response.resultCode === 0) {
+        let {id, email, login} = response.data;
+        dispatch(SetUserDataAC(id, email, login, true));
+        dispatch(SetLoadingAC(false))
+    }
 };
 
 export const logInUserThunk = (email: string, password: string, rememberMe: boolean) =>
-    (dispatch: any) => {
-        authApi.logIn(email, password, rememberMe).then((data) => {
-                if (data.resultCode === 0) {
-                    dispatch(authUserThunk())
-                } else {
-                    let message = data.messages.length > 0 ? data.messages[0] : 'something went wrong '
-                    dispatch(stopSubmit('login', {_error: message}))
-                }
-            }
-        )
+    async (dispatch: any) => {
+        let response = await authApi.logIn(email, password, rememberMe)
+        if (response.resultCode === 0) {
+            dispatch(authUserThunk())
+        } else {
+            let message = response.messages.length > 0 ? response.messages[0] : 'something went wrong '
+            dispatch(stopSubmit('login', {_error: message}))
+        }
+
+
     };
 
 export const logOutUserThunk = () =>
-    (dispatch: DispatchType) => {
-        authApi.logOut().then((data) => {
-            if (data.data.resultCode === 0) {
-                let {id, email, login} = data.data;
-                dispatch(SetUserDataAC(id, email, login, false))
-            }
-        });
+    async (dispatch: DispatchType) => {
+        let response = await authApi.logOut()
+        if (response.data.resultCode === 0) {
+            let {id, email, login} = response.data;
+            dispatch(SetUserDataAC(id, email, login, false))
+        }
     };
