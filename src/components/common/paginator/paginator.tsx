44a9b9848013e13ea0240cf78f5c8b8d6c9bd349
Yusup <hunzah9@mail.ru>
2023-06-29 12:52:52 +0300

    commit

--- src/components/common/paginator/paginator.tsx
+++ src/components/common/paginator/paginator.tsx
@@ -1,43 +1,75 @@
 import s from './paginator.module.css';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 type PropsType = {
-    onPageChanged: (p: number) => void
-    totalItemsCount: number
-    pageSize: number
-    currentPage: number
-    portionSize?:number
-}
+    onPageChanged: (p: number) => void;
+    totalItemsCount: number;
+    pageSize: number;
+    currentPage: number;
+    portionSize?: number;
+};
 
 export const Paginator = (props: PropsType) => {
-    const {onPageChanged, totalItemsCount, pageSize, currentPage,portionSize = 10} = props
+    const {
+        onPageChanged,
+        totalItemsCount,
+        pageSize,
+        currentPage,
+        portionSize = 10,
+    } = props;
 
-    let pagesCount = Math.ceil(totalItemsCount / pageSize)
-    let pages = []
+    // Вычисляем количество страниц
+    const pagesCount = Math.ceil(totalItemsCount / pageSize);
+
+    // Создаем массив со всеми страницами
+    const pages = [];
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
 
-    const portionCount = Math.ceil(pagesCount/portionSize)
-    const [portionNumber , setPortionNumber] = useState<number>(1)
-    const leftPortionNumber = (portionNumber-1) * portionSize +1
-    const rightPortionNumber = portionNumber * portionSize +1
+    // Вычисляем количество порций страниц
+    const portionCount = Math.ceil(pagesCount / portionSize);
 
-    let prevDisable =  portionNumber > 1
-    let nextDisable =  portionNumber ===portionCount
-    return <div>
-            <button
-                onClick={()=>setPortionNumber(portionNumber-1)}
-             disabled={prevDisable}>prev</button>
+    // Определяем состояние текущей порции
+    const [portionNumber, setPortionNumber] = useState<number>(1);
 
-        {pages.filter(p=> p> leftPortionNumber && p > leftPortionNumber)
-            .map(p =>
-            <span key={p} className={currentPage === p ? s.selectedPage : ''}
-                  onClick={() => onPageChanged(p)}>
-                        {p}
-                    </span>)}
-        <button
-            onClick={()=>setPortionNumber(portionNumber-1)}
-            disabled={prevDisable}>next</button>
-    </div>
-}
+    // Вычисляем номера первой и последней страницы в текущей порции
+    const leftPageNumber = (portionNumber - 1) * portionSize + 1;
+    const rightPageNumber = Math.min(portionNumber * portionSize, pagesCount);
+
+    // Определяем, должна ли быть доступна кнопка "prev"
+    const prevDisabled = portionNumber === 1;
+
+    // Определяем, должна ли быть доступна кнопка "next"
+    const nextDisabled = portionNumber === portionCount;
+
+    return (
+        <div>
+            <button onClick={() => setPortionNumber(portionNumber - 1)} disabled={prevDisabled}>
+                prev
+            </button>
+
+            {/* Вычисляем номера первой и последней страницы в порции исходя из выбранной страницы */}
+            {pages
+                .filter((p) => p >= leftPageNumber && p <= rightPageNumber)
+                .map((p) => (
+                    <span
+                        key={p}
+                        className={currentPage === p ? s.selectedPage : ''}
+                        onClick={() => {
+                            // Вычисляем номер порции исходя из выбранной страницы
+                            const portionNumber = Math.ceil(p / portionSize);
+                            setPortionNumber(portionNumber);
+                            onPageChanged(p);
+                        }}
+                    >
+            {p}
+          </span>
+                ))}
+
+            <button onClick={() => setPortionNumber(portionNumber + 1)} disabled={nextDisabled}>
+                next
+            </button>
+        </div>
+    );
+};
